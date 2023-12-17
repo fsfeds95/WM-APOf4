fc677b93ee148d52a7e1e81af7c790f9f6d806e5
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2023-12-17 08:16:59 -0400

    Actualizar app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 //---------------------------------
 
 // Paso 3: Definir la ruta /backdrop que escuchará las peticiones
-app.get('/backdrop', async (req, res) => {
+app.get('/b', async (req, res) => {
   // Paso 4: Obtener el enlace de la imagen suministrada
   const imageUrlBackdrop = req.query.img;
 
@@ -28,21 +28,24 @@ app.get('/backdrop', async (req, res) => {
 
     // Paso 8: Cargar la marca de agua
     const watermarkBackdrop = await Jimp.read('wm-backdrop.png');
+    
+    // Paso 9: Escalar la marca de agua a 1280x720
+    watermarkBackdrop.scaleToFit(1280, 720)
 
-    // Paso 9: Aplicar la marca de agua a la imagen
+    // Paso 10: Aplicar la marca de agua a la imagen
     imageBackdrop.composite(watermarkBackdrop, 0, 0, {
       mode: Jimp.BLEND_SCREEN,
       opacitySource: 0.75
     });
 
-    // Paso 10: Ajustar la calidad de la imagen de salida al 100%
+    // Paso 11: Ajustar la calidad de la imagen de salida al 100%
     imageBackdrop.quality(100);
 
-    // Paso 11: Guardar la imagen de salida con el nombre "WM_AstroPeliculas.jpg"
+    // Paso 12: Guardar la imagen de salida con el nombre "WM_AstroPeliculas.jpg"
     const outputFileNameBackdrop = 'WM_AstroPeliculas.jpg';
     await imageBackdrop.writeAsync(outputFileNameBackdrop);
 
-    // Paso 12: Enviar la imagen al navegador
+    // Paso 13: Enviar la imagen al navegador
     res.set('Content-Type', Jimp.MIME_JPEG);
     res.sendFile(outputFileNameBackdrop, { root: __dirname });
   } catch (error) {
@@ -52,11 +55,17 @@ app.get('/backdrop', async (req, res) => {
 });
 
 //--------------------------------
+//----- ACA TERMINA BACKDROP -----
+//--------------------------------
+
+
+
+//--------------------------------
 //----- ACA COMIENZA POSTERS -----
 //--------------------------------
 
 // Paso 3: Definir la ruta /poster que escuchará las peticiones
-app.get('/poster', async (req, res) => {
+app.get('/p', async (req, res) => {
   // Paso 4: Obtener el enlace de la imagen suministrada
   const imageUrlPoster = req.query.img;
 
@@ -69,26 +78,29 @@ app.get('/poster', async (req, res) => {
     // Paso 6: Cargar la imagen desde el enlace
     const imagePoster = await Jimp.read(imageUrlPoster);
 
-    // Paso 7: Escalar la imagen a 1280x720
+    // Paso 7: Escalar la imagen a 720x1280px
     imagePoster.scaleToFit(1280, 720);
 
     // Paso 8: Cargar la marca de agua
     const watermarkPoster = await Jimp.read('wm-poster.png');
+    
+   // Paso 9: Escalar la imagen a 720x1280px
+    watermarkPoster.scaleToFit(720, 1280);
 
-    // Paso 9: Aplicar la marca de agua a la imagen
+    // Paso 10: Aplicar la marca de agua a la imagen
     imagePoster.composite(watermarkPoster, 0, 0, {
       mode: Jimp.BLEND_SCREEN,
       opacitySource: 0.75
     });
 
-    // Paso 10: Ajustar la calidad de la imagePostern de salida al 100%
+    // Paso 11: Ajustar la calidad de la imagePostern de salida al 100%
     imagePoster.quality(100);
 
-    // Paso 11: Guardar la imagen de salida con el nombre "WM_AstroPeliculas.jpg"
+    // Paso 12: Guardar la imagen de salida con el nombre "WM_AstroPeliculas.jpg"
     const outputFileNamePoster = 'WM_AstroPeliculas.jpg';
     await imagePoster.writeAsync(outputFileNamePoster);
 
-    // Paso 12: Enviar la imagen al navegador
+    // Paso 13: Enviar la imagen al navegador
     res.set('Content-Type', Jimp.MIME_JPEG);
     res.sendFile(outputFileNamePoster, { root: __dirname });
   } catch (error) {
@@ -97,9 +109,11 @@ app.get('/poster', async (req, res) => {
   }
 });
 
+//-------------------------------
+//----- ACA TERMINA POSTERS -----
+//-------------------------------
 
-
-// Paso 13: Escuchar en el puerto 8225
+// Paso 14: Escuchar en el puerto 8225
 app.listen(8225, () => {
   console.log('La aplicación está escuchando en el puerto 8225.');
 });
